280ec38be60359578dc9538b927d515b9b447753
Erik Sampayo <erik2887@gmail.com>
2012-12-15 12:41:12 -0500

    Updated Actor.lua

--- Actor.lua
+++ Actor.lua
@@ -1,11 +1,21 @@
 Actor = class:new()
 
-function Actor:init(world, x, y, w, h, mass, type)
+function Actor:init(world, x, y, w, h, mass, type, shape, radius)
 
 	self.body = love.physics.newBody(world, x, y, type)
-	self.shape = love.physics.newRectangleShape(w, h)
+	if shape == "rectangle" then
+		self.shape = love.physics.newRectangleShape(w, h)
+		self.s = "rectangle"
+		self.fixture = love.physics.newFixture(self.body, self.shape)
+	end
+	if shape =="circle" then
+		self.shape = love.physics.newCircleShape(radius)
+		self.radius = radius
+		self.s = "circle"	
+		self.fixture = love.physics.newFixture(self.body, self.shape)
+	end
 	self.mass = mass
-	self.fixture = love.physics.newFixture(self.body, self.shape)
+
 	
 end
 
@@ -14,12 +24,21 @@ function Actor:update(dt)
 end
 
 function Actor:draw()
-
-	love.graphics.polygon(
-		"fill",
-		self.body:getWorldPoints(self.shape:getPoints())
-	)
-
+	if self.s == "rectangle" then
+		love.graphics.polygon(
+			"fill",
+			self.body:getWorldPoints(self.shape:getPoints())
+		)
+	end
+	
+	if self.s == "circle" then
+		love.graphics.circle(
+			"fill",
+			self.body:getX(), 
+			self.body:getY(),
+			self.shape:getRadius()
+		)
+	end
 end
 
 function Actor:setMass(mass)
